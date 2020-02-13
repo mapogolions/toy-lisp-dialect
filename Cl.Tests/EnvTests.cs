7eaeb21b34befd3401d2b1ee0b6ac99be1e04b81
mapogolions <ivanovpavelalex45@gmail.com>
2020-02-14 02:39:56 +0500

    Refactoring

--- Cl.Tests/EnvTests.cs
+++ Cl.Tests/EnvTests.cs
@@ -1,4 +1,3 @@
-using System;
 using Cl.Types;
 using NUnit.Framework;
 
@@ -7,7 +6,6 @@ namespace Cl.Tests
     [TestFixture]
     public class EnvTests
     {
-        private IEnv _env;
         private ClBool _theTrue;
         private ClBool _theFalse;
         private ClSymbol _foo;
@@ -20,12 +18,6 @@ namespace Cl.Tests
             _theFalse = new ClBool(false);
             _foo = new ClSymbol("foo");
             _bar = new ClSymbol("bar");
-        }
-
-        [SetUp]
-        public void BeforeEach()
-        {
-            _env = new Env();
         }
 
         [Test]
@@ -99,23 +91,27 @@ namespace Cl.Tests
         [Test]
         public void Lookup_ByKey_ReturnValue()
         {
-            _env.Bind(_bar,  _theFalse);
+            var env = new Env();
+            env.Bind(_bar,  _theFalse);
 
-            Assert.That(_env.Lookup(_bar), Is.EqualTo(_theFalse));
+            Assert.That(env.Lookup(_bar), Is.EqualTo(_theFalse));
         }
 
         [Test]
         public void Bind_ExistingKey_ReassignValue()
         {
-            _env.Bind(_foo, _theTrue);
+            var env = new Env();
+            env.Bind(_foo, _theTrue);
 
-            Assert.That(_env.Bind(_foo, _theFalse), Is.True);
+            Assert.That(env.Bind(_foo, _theFalse), Is.True);
         }
 
         [Test]
         public void Bind_NotExistingKey_CreateNewValue()
         {
-            Assert.That(_env.Bind(new ClSymbol("a"), new ClBool(true)), Is.True);
+            var env = new Env();
+
+            Assert.That(env.Bind(_bar, _theFalse), Is.True);
         }
     }
 }
