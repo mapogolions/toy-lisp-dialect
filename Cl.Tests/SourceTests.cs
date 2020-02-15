b9b48c6b521dd75492e3063d2ada3313e10d6e74
mapogolions <ivanovpavelalex45@gmail.com>
2020-02-15 21:50:09 +0500

    Tested peek method of ISource

--- Cl.Tests/SourceTests.cs
+++ Cl.Tests/SourceTests.cs
@@ -6,6 +6,33 @@ namespace Cl.Tests
     public class SourceTests
     {
         [Test]
+        public void Peek_Return_MinusOne_WhenSourceIsEmpty()
+        {
+            using var source = new Source(string.Empty);
+            Assert.That(source.Peek(), Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Peek_FullInnerBuffer()
+        {
+            using var source = new Source("ab");
+            source.Peek();
+            source.Peek();
+
+            Assert.That((char) source.Read(), Is.EqualTo('a'));
+            Assert.That((char) source.Read(), Is.EqualTo('b'));
+        }
+
+        [Test]
+        public void Peek_ReturnValueFromStreamAndSaveValueToInnerBuffer()
+        {
+            using var source = new Source("foo");
+
+            Assert.That((char) source.Peek(), Is.EqualTo('f'));
+            Assert.That((char) source.Read(), Is.EqualTo('f'));
+        }
+
+        [Test]
         public void Read_Return_MinusOne_WhenSourceCodeIsEmpty()
         {
             using var source = new Source(string.Empty);
