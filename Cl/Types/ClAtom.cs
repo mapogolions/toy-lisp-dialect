a841c58d806bed541a6869af5fc62b2dce5437ce
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-06 22:23:39 +0500

    Refactoring

--- Cl/Types/ClAtom.cs
+++ Cl/Types/ClAtom.cs
@@ -2,8 +2,8 @@ namespace Cl.Types
 {
     public abstract class ClAtom<T> : IClObj
     {
-        public T Value { get; }
-
         public ClAtom(T value) => Value = value;
+
+        public T Value { get; }
     }
 }
