ee1c97e44687d6f0bd0b118808148c3663610007
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-07 21:41:32 +0500

    Add extension methods for IClObj

---
+++ Cl/Extensions/IClObjOps.cs
@@ -0,0 +1,23 @@
+using Cl.Types;
+
+namespace Cl.Constants
+{
+    public static class IClObjOps
+    {
+        public static bool IsSelfEvaluating(this IClObj expr) => expr is ClPair || expr is ClSymbol;
+        public static bool IsVariable(this IClObj expr) => expr.IsSymbol();
+        public static bool IsSymbol(this IClObj expr) => expr is ClSymbol;
+        public static bool IsTaggedList(this IClObj expr, ClSymbol tag) => expr is ClPair cell && cell.Car == tag;
+        public static bool IsQuoted(this IClObj expr) => expr.IsTaggedList(ClSymbol.Quote);
+        public static bool IsAssignment(this IClObj expr) => expr.IsTaggedList(ClSymbol.Set);
+        public static bool IsDefinition(this IClObj expr) => expr.IsTaggedList(ClSymbol.Define);
+        public static bool IsIf(this IClObj expr) => expr.IsTaggedList(ClSymbol.If);
+        public static bool IsAnd(this IClObj expr) => expr.IsTaggedList(ClSymbol.And);
+        public static bool IsOr(this IClObj expr) => expr.IsTaggedList(ClSymbol.Or);
+        public static bool IsLet(this IClObj expr) => expr.IsTaggedList(ClSymbol.Let);
+        public static bool IsCond(this IClObj expr) => expr.IsTaggedList(ClSymbol.Cond);
+        public static bool IsBegin(this IClObj expr) => expr.IsTaggedList(ClSymbol.Begin);
+        public static bool IsLambda(this IClObj expr) => expr.IsTaggedList(ClSymbol.Lambda);
+        public static bool IsEmptyList(this IClObj expr) => expr == Nil.Given;
+    }
+}
