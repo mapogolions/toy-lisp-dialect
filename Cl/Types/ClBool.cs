6b7d9110d3f2b486cc734bcdf72fa4392b2ade71
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-07 21:40:55 +0500

    Add static members

--- Cl/Types/ClBool.cs
+++ Cl/Types/ClBool.cs
@@ -2,9 +2,9 @@ namespace Cl.Types
 {
     public class ClBool : ClAtom<bool>
     {
+        private ClBool(bool flag) : base(flag) { }
+
         public static ClBool True = new ClBool(true);
         public static ClBool False = new ClBool(false);
-
-        private ClBool(bool flag) : base(flag) { }
     }
 }
--- Cl/Types/ClSymbol.cs
+++ Cl/Types/ClSymbol.cs
@@ -3,5 +3,16 @@ namespace Cl.Types
     public class ClSymbol : ClAtom<string>
     {
         public ClSymbol(string name) : base(name) { }
+
+        public static ClSymbol And = new ClSymbol("and");
+        public static ClSymbol If = new ClSymbol("if");
+        public static ClSymbol Or = new ClSymbol("or");
+        public static ClSymbol Let = new ClSymbol("let");
+        public static ClSymbol Cond = new ClSymbol("cond");
+        public static ClSymbol Begin = new ClSymbol("begin");
+        public static ClSymbol Set = new ClSymbol("set!");
+        public static ClSymbol Define = new ClSymbol("define");
+        public static ClSymbol Quote = new ClSymbol("quote");
+        public static ClSymbol Lambda = new ClSymbol("lambda");
     }
 }
