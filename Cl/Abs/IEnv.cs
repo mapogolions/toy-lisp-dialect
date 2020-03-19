b46a537567f53b966f7dc1bcca2383e195ab1b15
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-19 21:18:38 +0500

    Update signature

--- Cl/Abs/IEnv.cs
+++ Cl/Abs/IEnv.cs
@@ -6,28 +6,28 @@ namespace Cl.Abs
 {
     public interface IEnv
     {
-        bool Bind(IClObj symbol, IClObj obj);
-        IClObj Lookup(IClObj symbol);
-        bool Assign(IClObj symbol, IClObj obj);
+        bool Bind(ClSymbol symbol, IClObj obj);
+        IClObj Lookup(ClSymbol symbol);
+        bool Assign(ClSymbol symbol, IClObj obj);
     }
 
     public class Env : IEnv
     {
         private readonly IEnv _frame;
-        private readonly IDictionary<IClObj, IClObj> _bindings = new Dictionary<IClObj, IClObj>();
+        private readonly IDictionary<ClSymbol, IClObj> _bindings = new Dictionary<ClSymbol, IClObj>();
 
         public Env(IEnv frame = null)
         {
             _frame = frame;
         }
 
-        public bool Bind(IClObj symbol, IClObj obj)
+        public bool Bind(ClSymbol symbol, IClObj obj)
         {
             _bindings[symbol] = obj;
             return true;
         }
 
-        public IClObj Lookup(IClObj symbol)
+        public IClObj Lookup(ClSymbol symbol)
         {
             if (_bindings.TryGetValue(symbol, out var obj))
                 return obj;
@@ -37,7 +37,7 @@ namespace Cl.Abs
             return result;
         }
 
-        public bool Assign(IClObj symbol, IClObj obj)
+        public bool Assign(ClSymbol symbol, IClObj obj)
         {
             if (_bindings.ContainsKey(symbol))
                 return Bind(symbol, obj);
