8d99110651e55a85c387b60233614a5f5379f2ff
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-24 23:55:49 +0500

    Add some test for eval begin feat.

---
+++ Cl.Tests/EvaluatorTests/EvaBeginTests.cs
@@ -0,0 +1,28 @@
+using Cl.Abs;
+using Cl.Types;
+using NUnit.Framework;
+
+namespace Cl.Tests.EvaluatorTests
+{
+    [TestFixture]
+    public class EvalBeginTests
+    {
+        [Test]
+        public void EvalBegin_ReturnLastEvaluatedValue()
+        {
+            var evaluator = new Evaluator(new Env());
+            var expr = BuiltIn.ListOf(ClSymbol.Begin, ClBool.False, ClBool.True);
+
+            Assert.That(evaluator.EvalBegin(expr), Is.EqualTo(ClBool.True));
+        }
+
+        [Test]
+        public void EvalBegin_ReturnNil_WhenTailIsEmptyList()
+        {
+            var evaluator = new Evaluator(new Env());
+            var expr = BuiltIn.ListOf(ClSymbol.Begin); // (begin) -> (begin . nil)
+
+            Assert.That(evaluator.EvalBegin(expr), Is.EqualTo(Nil.Given));
+        }
+    }
+}
