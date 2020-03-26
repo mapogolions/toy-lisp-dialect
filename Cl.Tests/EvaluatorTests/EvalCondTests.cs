aba23a4935e8bda0f27dcceb8c0402e045a2e286
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-27 02:45:24 +0500

    Start testing eval cond feat.

---
+++ Cl.Tests/EvaluatorTests/EvalCondTests.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using Cl.Abs;
+using Cl.Types;
+using NUnit.Framework;
+
+namespace Cl.Tests.EvaluatorTests
+{
+    [TestFixture]
+    public class EvalCondTest
+    {
+        //[Test]
+        //public void EvalCond_ReturnNil_WhenEachClausePredicateIsFalse()
+        //{
+        //    var evaluator = new Evaluator(new Env());
+        //    var clause1 = BuiltIn.ListOf(ClBool.False, new ClChar('a'));
+        //    var clause2 = BuiltIn.ListOf(Nil.Given, new ClChar('b'));
+        //    var expr = BuiltIn.ListOf(ClSymbol.Cond, clause1, clause2);
+
+        //    Assert.That(evaluator.Eval(expr), Is.EqualTo(Nil.Given));
+        //}
+        //[Test]
+        //public void EvalCond_ReturnLastEvaluatedResult()
+        //{
+        //    var evaluator = new Evaluator(new Env());
+        //    var elseClause = BuiltIn.ListOf(ClSymbol.Else, BuiltIn.ListOf(new ClChar('a'), new ClChar('b')));
+        //    var expr = BuiltIn.ListOf(ClSymbol.Cond, elseClause);
+
+        //    Assert.That(evaluator.Eval(expr), Is.EqualTo(new ClChar('b')));
+        //}
+
+        [Test]
+        public void EvalCond_ReturnResultOfElseClause_WhenItIsSingle()
+        {
+            var evaluator = new Evaluator(new Env());
+            var result = new ClFixnum(10);
+            var elseClause = BuiltIn.ListOf(ClSymbol.Else, result);
+            var expr = BuiltIn.ListOf(ClSymbol.Cond, elseClause);
+
+            Assert.That(evaluator.Eval(expr), Is.EqualTo(result));
+        }
+
+        [Test]
+        public void EvalCond_ThrowException_WhenElseClauseIsNotLast()
+        {
+            var evaluator = new Evaluator(new Env());
+            var elseClause = BuiltIn.ListOf(ClSymbol.Else, ClBool.True);
+            var expr = BuiltIn.ListOf(ClSymbol.Cond, elseClause, BuiltIn.ListOf(ClSymbol.Else, ClBool.True));
+
+            Assert.That(() => evaluator.Eval(expr), 
+                Throws.InvalidOperationException.With.Message.EqualTo("Else clause is not last condition"));
+        }
+
+        [Test]
+        public void EvalCond_ReturnFalse_WhenParamsAreMissed()
+        {
+            var evaluator = new Evaluator(new Env());
+            var expr = BuiltIn.ListOf(ClSymbol.Cond);
+
+            Assert.That(evaluator.Eval(expr), Is.EqualTo(ClBool.False));
+        }
+    }
+}
