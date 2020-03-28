dbe2737f183244271448d5321d5690faf3734467
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-28 22:43:35 +0500

    Test lazy behaviour of logic or

--- Cl.Tests/EvaluatorTests/EvalOrTests.cs
+++ Cl.Tests/EvaluatorTests/EvalOrTests.cs
@@ -9,6 +9,20 @@ namespace Cl.Tests.EvaluatorTests
     public class EvalOrTests
     {
         [Test]
+        public void EvalOr_MustBeLazy()
+        {
+            var env = new Env();
+            var evaluator = new Evaluator(env);
+            var define = BuiltIn.ListOf(ClSymbol.Define, new ClSymbol("a"), new ClFixnum(2));
+            var expr = BuiltIn.ListOf(ClSymbol.Or, ClBool.True, define);
+
+            Assert.That(evaluator.Eval(expr), Is.EqualTo(ClBool.True));
+            Assert.That(() => env.Lookup(new ClSymbol("a")),
+                Throws.InvalidOperationException.With.Message.EquivalentTo("Unbound variable"));
+        }
+
+
+        [Test]
         public void EvalOr_ReturnFalse_WhenEachItemIsFalse()
         {
             var evaluator = new Evaluator(new Env());
