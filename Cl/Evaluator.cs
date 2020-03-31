b9322e1591a2f8c0028f51977f6f0b7c5bb0d749
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-31 20:48:56 +0500

    Add Try prefix

--- Cl/Evaluator.cs
+++ Cl/Evaluator.cs
@@ -16,17 +16,18 @@ namespace Cl
 
         public IClObj Eval(IClObj expr)
         {
+            // Console.WriteLine("here" + expr.GetType().Name);
             if (expr.IsSelfEvaluating()) return expr;
             if (expr.IsVariable()) return _env.Lookup(expr.Cast<ClSymbol>());
-            if (expr.IsAssignment()) return EvalAssigment(expr);
-            if (expr.IsDefinition()) return EvalDefinition(expr);
             if (expr.IsQuoted()) return BuiltIn.Tail(expr);
-            if (expr.IsIfThenElse()) return EvalIfThenElse(expr);
-            if (expr.IsAnd()) return EvalAnd(expr);
-            if (expr.IsOr()) return EvalOr(expr);
-            if (expr.IsBegin()) return EvalBegin(expr);
-            if (expr.IsLambda()) return EvalLambda(expr);
             if (expr.IsCond()) return Eval(TransformCond(expr));
+            if (TryEvalAssigment(expr, out var obj)) return obj;
+            if (TryEvalDefinition(expr, out obj)) return obj;
+            if (TryEvalIfThenElse(expr, out obj)) return obj;
+            if (TryEvalAnd(expr, out obj)) return obj;
+            if (TryEvalOr(expr, out obj)) return obj;
+            if (TryEvalBegin(expr, out obj)) return obj;
+            if (TryEvalLambda(expr, out obj)) return obj;
             throw new InvalidOperationException("Evaluation error");
         }
 
@@ -47,84 +48,107 @@ namespace Cl
                 TransformCond(BuiltIn.Tail(clauses)));
         }
 
-        public IClObj EvalBegin(IClObj expr)
+        public bool TryEvalBegin(IClObj expr, out IClObj obj)
         {
+            obj = Nil.Given;
+            if (!expr.IsBegin()) return false;
             var tail = BuiltIn.Tail(expr);
-            IClObj result = Nil.Given;
             while (tail != Nil.Given)
             {
-                result = Eval(BuiltIn.Head(tail));
+                obj = Eval(BuiltIn.Head(tail));
                 tail = BuiltIn.Tail(tail);
             }
-            return result;
+            return true;
         }
 
-        public IClObj EvalOr(IClObj expr)
+        public bool TryEvalOr(IClObj expr, out IClObj obj)
         {
-            // (or :expr-1 :expr-2 ... :expr-n) -> (and . (:expr-1 . (:expr-2 . (... (expr-n . nil))))
+            obj = ClBool.False;
+            if (!expr.IsOr()) return false;
             var tail = BuiltIn.Tail(expr);
             while (tail != Nil.Given)
             {
                 var result = Eval(BuiltIn.Head(tail));
-                if (result != Nil.Given && result != ClBool.False) return ClBool.True;
+                if (result != Nil.Given && result != ClBool.False)
+                {
+                    obj = ClBool.True;
+                    break;
+                }
                 tail = BuiltIn.Tail(tail);
             }
-            return ClBool.False;
+            return true;
         }
 
-        public IClObj EvalAnd(IClObj expr)
+        public bool TryEvalAnd(IClObj expr, out IClObj obj)
         {
-            // (and :expr-1 :expr-2 ... :expr-n) -> (and . (:expr-1 . (:expr-2 . (... (expr-n . nil))))
+            obj = ClBool.True;
+            if (!expr.IsAnd()) return false;
             var tail = BuiltIn.Tail(expr);
             while (tail != Nil.Given)
             {
                 var result = Eval(BuiltIn.Head(tail));
-                if (result == Nil.Given || result == ClBool.False) return ClBool.False;
+                if (result == Nil.Given || result == ClBool.False)
+                {
+                    obj = ClBool.False;
+                    break;
+                }
                 tail = BuiltIn.Tail(tail);
             }
-            return ClBool.True;
+            return true;
         }
 
         // TODO: add tests
-        public IClObj EvalLambda(IClObj expr)
+        public bool TryEvalLambda(IClObj expr, out IClObj obj)
         {
+            obj = Nil.Given;
+            if (!expr.IsLambda()) return false;
             // (lambda (a b c) :body-expr)
             var args = BuiltIn.Cadr(expr);
             var body = BuiltIn.Cddr(expr);
-            return new ClProc(args.Cast<ClCell>(), body);
+            obj = new ClProc(args.Cast<ClCell>(), body);
+            return true;
         }
 
-        // (if :expr :expr :expr) -> (if . (:expr . (:expr . (:expr . nil)))) else branch is provided
-        // (if :expr :expr) -> (if . (:expr . (:expr . nil))) without else branch
-        public IClObj EvalIfThenElse(IClObj expr)
+        public bool TryEvalIfThenElse(IClObj expr, out IClObj obj)
         {
+            obj = Nil.Given;
+            if (!expr.IsIfThenElse()) return false;
             var result = Eval(BuiltIn.Second(expr));
-            if (result != Nil.Given && result != ClBool.False)  return Eval(BuiltIn.Third(expr));
+            if (result != Nil.Given && result != ClBool.False)
+            {
+                obj = Eval(BuiltIn.Third(expr));
+                return true;
+            }
             var elseBranch = BuiltIn.Cdddr(expr);
-            return elseBranch ==  Nil.Given ? Nil.Given : Eval(BuiltIn.First(elseBranch));
+            obj = elseBranch ==  Nil.Given ? Nil.Given : Eval(BuiltIn.First(elseBranch));
+            return true;
         }
 
-        public IClObj EvalDefinition(IClObj expr)
+        public bool TryEvalDefinition(IClObj expr, out IClObj obj)
         {
             // (define a 10) -> (define . (a . (10 . nil)))
             // (define a b) -> (define . (a . (b . nil)))
             // (define a a) -> (define . (a . (a . nil))) throw exception
             // TODO: (define keyword 10) i.e. (define lambda 10) Need reject
+            obj = Nil.Given;
+            if (!expr.IsDefinition()) return false;
             var identifier = BuiltIn.Second(expr).Cast<ClSymbol>();
-            var obj = Eval(BuiltIn.Third(expr));
-            _env.Bind(identifier, obj);
-            return Nil.Given;
+            var result = Eval(BuiltIn.Third(expr));
+            _env.Bind(identifier, result);
+            return true;
         }
 
-        public IClObj EvalAssigment(IClObj expr)
+        public bool TryEvalAssigment(IClObj expr, out IClObj obj)
         {
             // (set! a 10) -> (set! . (a . (10 . nil)))
             // (set! a b) -> (set! . (a . (b . nil)))
             // (set! a a) -> (set! . (a . (a . nil))) reassign
+            obj = Nil.Given;
+            if (!expr.IsAssignment()) return false;
             var identifier = BuiltIn.Second(expr).Cast<ClSymbol>();
-            var obj = Eval(BuiltIn.Third(expr));
-            _env.Assign(identifier, obj);
-            return Nil.Given;
+            var result = Eval(BuiltIn.Third(expr));
+            _env.Assign(identifier, result);
+            return true;
         }
     }
 }
