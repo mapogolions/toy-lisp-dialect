d4620d8ff386347baf543bec708b14956c03e12c
mapogolions <ivanovpavelalex45@gmail.com>
2020-03-31 21:08:31 +0500

    Remove outdated artifacts

--- Cl/Repl.cs
+++ Cl/Repl.cs
@@ -1,22 +1,18 @@
 using System;
-using System.Collections.Generic;
 using Cl.Abs;
-using Cl.Types;
 
 namespace Cl
 {
     public class Repl
     {
         private readonly IEnv _env;
-        private readonly IDictionary<string, IClObj> _symbolsTable;
 
-        public Repl(IEnv env, IDictionary<string, IClObj> symbolsTable)
+        public Repl(IEnv env)
         {
             _env = env;
-            _symbolsTable = symbolsTable;
         }
 
-        public Repl() : this(new Env(), new Dictionary<string, IClObj>()) { }
+        public Repl() : this(new Env()) { }
 
         public void Start(string sign = ">")
         {
