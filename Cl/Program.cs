27c1a190aa9fa7d6edaf625ca4415dd85444badb
mapogolions <ivanovpavelalex45@gmail.com>
2020-04-12 22:15:43 +0500

    Add example

--- Cl/Program.cs
+++ Cl/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Cl
+﻿using System;
+using Cl.Abs;
+
+namespace Cl
 {
     class Program
     {
         static void Main(string[] args)
         {
-            new Repl().Start();
+            var source = @"
+            (define my-first-procedure
+              (lambda (x)
+                (if x 11 10)))
+            (my-first-procedure #t)
+            ";
+            var reader = new Reader(source);
+            var evaluator = new Evaluator(new Env(BuiltIn.Env));
+            var result = evaluator.Eval(reader.Read());
+            Console.WriteLine(result);
+            // new Repl().Start();
         }
     }
 }
