e108ba252de5baedc0237614a9b2047bf98277a9
mapogolions <ivanovpavelalex45@gmail.com>
2020-04-17 23:49:48 +0500

    Reproduce bug

--- Cl/Program.cs
+++ Cl/Program.cs
@@ -13,7 +13,17 @@ namespace Cl
 
                 (my-first-procedure #t)
             ";
-            var reader = new Reader(source);
+            // Todo
+            var closure = @"
+                (define x 10)
+                (define f
+                    (lambda (x)
+                        (lambda () x)))
+
+                (define g (f 10))
+                (g)
+            ";
+            var reader = new Reader(closure);
             var evaluator = new Evaluator(new Env(BuiltIn.Env));
             var result = evaluator.Eval(reader.Read());
             Console.WriteLine(result);
