6bdb59eec301e5823651bab6d90fd476ec2ea709
mapogolions <ivanovpavelalex45@gmail.com>
2020-04-17 23:50:31 +0500

    Fix warning

--- Cl/Program.cs
+++ Cl/Program.cs
@@ -6,14 +6,7 @@ namespace Cl
     {
         static void Main(string[] args)
         {
-            var source = @"
-                (define my-first-procedure
-                    (lambda (x)
-                        (if x 11 10)))
-
-                (my-first-procedure #t)
-            ";
-            // Todo
+            // TODO
             var closure = @"
                 (define x 10)
                 (define f
