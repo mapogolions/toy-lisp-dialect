97b418dbe8ff935a60880fbe5c79a2f960ea6824
mapogolions <ivanovpavelalex45@gmail.com>
2020-04-21 00:55:41 +0500

    Update example

--- Cl/Program.cs
+++ Cl/Program.cs
@@ -6,17 +6,17 @@ namespace Cl
     {
         static void Main(string[] args)
         {
-            // TODO
-            var closure = @"
-                (define x 10)
+            var lexicalEnvironment = @"
                 (define f
                     (lambda (x)
-                        (lambda () x)))
+                        (lambda (x)
+                            (lambda () x))))
 
                 (define g (f 10))
-                (g)
+                (define h (g 11))
+                (h)
             ";
-            var reader = new Reader(closure);
+            var reader = new Reader(lexicalEnvironment);
             var evaluator = new Evaluator(new Env(BuiltIn.Env));
             var result = evaluator.Eval(reader.Read());
             Console.WriteLine(result);
