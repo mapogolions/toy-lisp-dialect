5462f681d8768a947750d238df9e8a9ff16c9735
mapogolions <ivanovpavelalex45@gmail.com>
2020-04-22 22:49:52 +0500

    Add fixtures

---
+++ Cl.Tests/Fixtures.cs
@@ -0,0 +1,18 @@
+using Cl.Types;
+
+namespace Cl.Tests
+{
+    internal static class Var
+    {
+        internal static ClSymbol Foo = new ClSymbol("foo");
+        internal static ClSymbol Bar = new ClSymbol("bar");
+        internal static ClSymbol Fn = new ClSymbol("fn");
+    }
+
+    internal static class Value
+    {
+        internal static ClString Foo = new ClString("foo");
+        internal static ClString Bar = new ClString("bar");
+        internal static ClFixnum One = new ClFixnum(1);
+    }
+}
