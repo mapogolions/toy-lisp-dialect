79fe62b1537c1da7e5fdbbbd57ae5fc09b8329ec
mapogolions <ivanovpavelalex45@gmail.com>
2020-08-11 20:34:24 +0500

    restructuring contracts

---
+++ Cl/Contracts/Context.cs
@@ -0,0 +1,30 @@
+using Cl.Types;
+
+namespace Cl.Contracts
+{
+    public class Context : IContext
+    {
+        public IClObj Value { get; }
+        public IEnv Env { get; }
+
+        public Context(IClObj result, IEnv env)
+        {
+            Value = result;
+            Env = env;
+        }
+
+        public Context(IEnv env) : this(Nil.Given, env) { }
+
+        public Context() : this(new Env()) { }
+
+        public IContext FromResult(IClObj result) => new Context(result, Env);
+
+        public IContext FromEnv(IEnv env) => new Context(Value, env);
+
+        public void Deconstruct(out IClObj result, out IEnv env)
+        {
+            result = Value;
+            env = Env;
+        }
+    }
+}
--- Cl/Contracts/IContext.cs
+++ Cl/Contracts/IContext.cs
@@ -12,30 +12,4 @@ namespace Cl.Contracts
 
         void Deconstruct(out IClObj result, out IEnv env);
     }
-
-    public class Context : IContext
-    {
-        public IClObj Value { get; }
-        public IEnv Env { get; }
-
-        public Context(IClObj result, IEnv env)
-        {
-            Value = result;
-            Env = env;
-        }
-
-        public Context(IEnv env) : this(Nil.Given, env) { }
-
-        public Context() : this(new Env()) { }
-
-        public IContext FromResult(IClObj result) => new Context(result, Env);
-
-        public IContext FromEnv(IEnv env) => new Context(Value, env);
-
-        public void Deconstruct(out IClObj result, out IEnv env)
-        {
-            result = Value;
-            env = Env;
-        }
-    }
 }
