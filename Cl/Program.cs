a87b9061a3acb6eb0e014f9ce527247b28a1b0e1
mapogolions <ivanovpavelalex45@gmail.com>
2022-03-28 23:04:59 +0500

    update entry point

--- Cl/Program.cs
+++ Cl/Program.cs
@@ -1,18 +1,10 @@
-﻿using Cl;
-using Cl.Readers;
+﻿using Cl.Readers;
 
 namespace Cl
 {
     internal static class Program
     {
-        // internal static void Main() =>
-        //     new Repl(">>>", s => new Reader(s))
-        //         .Start(new Context(BuiltIn.Env));
-
-        internal static void Main()
-        {
-            var reader = new Readers.Reader(new ClObjReader());
-            new Repl(">>>", reader).Start(new Context(BuiltIn.Env));
-        }
+        internal static void Main() =>
+            new Repl(">", new Reader()).Start(new Context(BuiltIn.Env));
     }
 }
